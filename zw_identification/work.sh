c88cf612d177b2c3142aca26ffc2697caafdf08c
Luohao Xu <luohaox@gmail.com>
2020-04-13 17:24:49 +0200

    Update work.sh

--- zw_identification/work.sh
+++ zw_identification/work.sh
@@ -1,4 +1,4 @@
 # create a list containing the paths of genomes and alignments
 cat b10k.list | while read id; do echo -e "$id\t/hwfssz5/ST_DIVERSITY/B10K/PMO/2.Family_level/01.Assembly/$id/$id.genomic.fa\t/zfssz3/ST_DIVERSITY/F16ZQSB1SY2933/PMO/B10K_psmc/01.famliy_level/01.recal.bam/$id.clean.filter.sort.dedup.AddGroup.realign.recal.bam";done > b10k.list.genome-bam
 
-b10k.list.genome-bam | while read id genome bam; do sh half-coverage $genome $bam; done
+cat b10k.list.genome-bam | while read id genome bam; do sh half-coverage.sh $genome $bam; done
