fbf8303b82b9f93a6615252b1f032af7113c6e1c
Luohao Xu <luohaox@gmail.com>
2020-04-13 23:27:44 +0200

    Update z_seq.sh

--- zw_identification/z_seq.sh
+++ zw_identification/z_seq.sh
@@ -18,5 +18,6 @@ show-coords -H -c -l -o -r -T ${ref}-$spe.delta.filt > ${ref}-$spe.delta.filt.co
 
 cat  ${ref}-$spe.delta.filt.coords  | grep -w -f $ref_z - | awk '$5>500' | awk '$3<$4{print $13"\t"$3"\t"$4}$3>$4{print $13"\t"$4"\t"$3}' |  bedtools sort -i - | bedtools merge -i - -d 10000 |  awk 'BEGIN{while(getline < "'$spe'.fai"){b[$1]=$2}}{a[$1]+=$3-$2+1}END{for(i in a){print i"\t"a[i]/b[i]*100"\t"b[i]}}' > ${ref}-$spe.delta.filt.coords.Z-perc
 
+Rscipt Z-perc.dist.R ${ref}-$spe.delta.filt.coords.Z-perc ${ref}-$spe.delta.filt.coords.Z-perc.dist.pdf
 cutoff=60 # the cutoff is to be decided according to the distribution of ${ref}-$spe.delta.filt.coords.Z-perc
 cat ${ref}-$spe.delta.filt.coords.Z-perc | awk -v cutoff=$cutoff '$3>cutoff' > $spe.z-list
