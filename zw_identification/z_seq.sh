40ac876e138cdb2a07d8db77ea27094954c446c1
Luohao Xu <luohaox@gmail.com>
2020-05-31 10:02:39 +0200

    Update z_seq.sh

--- zw_identification/z_seq.sh
+++ zw_identification/z_seq.sh
@@ -21,9 +21,9 @@ cat  ${ref}-$spe.delta.filt.coords  | grep -w -f $ref_z - | awk '$5>500' | awk '
 # %aligned by autosome
 cat  ${ref}-$spe.delta.filt.coords  | grep -v -w -f $ref_z - | awk '$5>500' | awk '$13>20000' | awk '$3<$4{print $13"\t"$3"\t"$4}$3>$4{print $13"\t"$4"\t"$3}' |  bedtools sort -i - | bedtools merge -i - -d 10000 |  awk 'BEGIN{while(getline < "'$spe'.fai"){b[$1]=$2}}{a[$1]+=$3-$2+1}END{for(i in a){print i"\t"a[i]/b[i]*100"\t"b[i]}}' > ${ref}-$spe.delta.filt.coords.A-perc
 
-cat ${ref}-$spe.delta.filt.coords.Z-perc | awk 'BEGIN{while(getline < "'${ref}'-'$spe'.delta.filt.coords.A-perc"){a[$1]=$3}}{print $0"\t"$3-a[$1]}' > ${ref}-$spe.delta.filt.coords.ZsubA-perc
+cat ${ref}-$spe.delta.filt.coords.Z-perc | awk 'BEGIN{while(getline < "'${ref}'-'$spe'.delta.filt.coords.A-perc"){a[$1]=$2}}{print $0"\t"$2-a[$1]}' > ${ref}-$spe.delta.filt.coords.ZsubA-perc
 
 
 Rscipt Z-perc.dist.R ${ref}-$spe.delta.filt.coords.Z-perc ${ref}-$spe.delta.filt.coords.Z-perc.dist.pdf
 cutoff=60 # the cutoff is to be decided according to the distribution of ${ref}-$spe.delta.filt.coords.Z-perc
-cat ${ref}-$spe.delta.filt.coords.Z-perc | awk -v cutoff=$cutoff '$3>cutoff' > $spe.z-list
+cat ${ref}-$spe.delta.filt.coords.Z-perc | awk -v cutoff=$cutoff '$2>cutoff' > $spe.z-list
